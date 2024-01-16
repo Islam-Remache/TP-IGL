f93461b299db48d1d2a70c45438a423a9341b540
Abderraouf Selidja <raouf.selidja@gmail.com>
2024-01-15 20:51:22 -1100

    HomePage + LoginSignup

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -3,6 +3,8 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { Recherche } from "./Recherche";
 import { Correction } from "./Correction";
 import { Statistics } from "./Statistics";
+import { Home } from "./Home";
+import { LoginSignup } from "./LoginSignup";
 function App() {
   return (
     <div className="App">
@@ -11,6 +13,8 @@ function App() {
           <Route path="/recherche" element={<Recherche />} />
           <Route path="/correction" element={<Correction />} />
           <Route path="/statistics" element={<Statistics />} />
+          <Route path="/Home" element={<Home />} />
+          <Route path="/LoginSignup" element={<LoginSignup />} />
           <Route path="*" element={<h1>page not found</h1>} />
         </Routes>
       </Router>
---
+++ Frontend/src/Home.js
@@ -0,0 +1,39 @@
+import { Link } from "react-router-dom";
+import "./Home.css";
+export const Home = () => {
+  return (
+    <div>
+      {/* <!-- Start Header --> */}
+      <header>
+        <Link to={"/"} className="logo">
+          <span>Sci</span>Search
+        </Link>
+      </header>
+      {/* <!-- End Header --> */}
+      {/* <!-- Start Landing --> */}
+      <div className="landing">
+        <div className="container">
+          <div className="overlay"></div>
+          <div className="text">
+            <h2>
+              Rechercher Votre
+              <br />
+              <span>Articles</span>
+            </h2>
+            <p>Explorez le monde de la connaissance scientifique en un clic!</p>
+            <div className="links">
+              <Link className="link" to={"/LoginSignup"}>
+                inscrire
+              </Link>
+              <Link className="link" to={"/LoginSignup"}>
+                connexion
+              </Link>
+            </div>
+          </div>
+        </div>
+      </div>
+      {/* Main Js File  */}
+      <script src="js/script.js"></script>
+    </div>
+  );
+};
