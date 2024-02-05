e12b4dab51434ecb38c93fe75e35e839fb7a164b
Islam-Remache <IslamRmh21@gmail.com>
2024-02-05 17:16:03 +0100

    modified search and  axios commit

--- Frontend/src/UploadedArticles.js
+++ Frontend/src/UploadedArticles.js
@@ -1,8 +1,18 @@
 import { IoIosAddCircleOutline } from "react-icons/io";
 import "./Uploaded.css";
-import React from "react";
+import React, { useEffect } from "react";
+import axios from "./api/axios";
 import Uploaded from "./Uploaded.json"; //favoris json file which contain all the favoris articles of the current user
+import { useState } from "react";
 export const UploadedArticles = (props) => {
+  const [Uploaded ,setUploaded]=useState([]);
+  useEffect(()=>{
+    console.log('jj')
+    axios.get("http://localhost:8000/ArticlesManager/getNonValid/").then((res) => {
+      setUploaded(res.data);
+      console.log(res.data)
+    });
+  },[])
   return (
     <div id="all">
       <div className="head">
@@ -19,16 +29,17 @@ export const UploadedArticles = (props) => {
         <input type="file" id="file-input" class="file-input" />
       </div>
       <div className="Uploaded">
-        {Uploaded.map((record) => {
+        {Uploaded['Articles Found']?.map((record) => {
+          record = record['_source'];
           return (
             <div className="card-container">
               <img alt="article-img" src={require("./file.png")} />
               <div className="content">
-                <h3>{record.title}</h3>
-                <p>{record.author}</p>
+                <h3>{record.Titre}</h3>
+                <p>{record.Auteurs[0]['NomComplet']}</p>
               </div>
               <div className="info">
-                <span title={record.tags}>{record.tags}</span>
+                <span title={record.MotsCle[0]}>{record.MotsCle[1]}</span>
               </div>
             </div>
           );
